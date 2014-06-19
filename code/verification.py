14d2ea279a2bf4eaba4a4e97c6c64c19103f375c
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-06-19 12:13:54 +0200

    updates

--- code/verification.py
+++ code/verification.py
@@ -67,35 +67,34 @@ class Verification(base.BaseEstimator):
 
     def predict(self, dataset):
         texts, titles, authors = dataset
-        for i, j in combinations(range(len(authors)), 2):
+        for i, j in combinations(range(len(titles)), 2):
             vec_i, title_i, author_i = self.X[i], titles[i], authors[i]
             vec_j, title_j, author_j = self.X[j], titles[j], authors[j]
             similarities = []
-            for k in range(len(authors)):
+            for k in range(len(titles)):
                 text, title, author = texts[k], titles[k], authors[k]
                 if author not in (author_i, author_j):
                     similarities.append((k, author, min_max(self.X[i], self.X[k])))
             similarities.sort(key=lambda s: s[-1], reverse=True)
-            if similarities:
-                indexes, imposters, _ = zip(*similarities[:self.imposters])
-                X = self.X[list(indexes)]
-                closest = []
-                sigmas = np.zeros(self.iterations)
-                for k in range(self.iterations):
-                    indices = np.random.randint(0, X.shape[1], size=self.rand_features)
-                    truncated_X = X[:,indices]
-                    similarities = []
-                    for idx, candidate in enumerate(imposters):
-                        similarities.append((candidate, min_max(truncated_X[idx], self.X[i])))
-                    similarities.append(('target', min_max(self.X[j,indices], self.X[i])))
-                    closest.append(max(similarities, key=lambda i: i[1])[0])
-                    sigma = closest.count("target") / float(len(closest))
-                    sigmas[k] = sigma
-                mean_sigma = sigmas.mean()
-                print "sigma for text: ", mean_sigma
-                same_author = True if mean_sigma >= self.sigma else False
-                print "%s (by %s) same author as %s (by %s) = %s" % (
-                    title_i, author_i, title_j, author_j, same_author)
+            indexes, imposters, _ = zip(*similarities[:self.imposters])
+            X = self.X[list(indexes)]
+            closest = []
+            sigmas = np.zeros(self.iterations)
+            for k in range(self.iterations):
+                indices = np.random.randint(0, X.shape[1], size=self.rand_features)
+                truncated_X = X[:,indices]
+                similarities = []
+                for idx, candidate in enumerate(imposters):
+                    similarities.append((candidate, min_max(truncated_X[idx], self.X[i,indices])))
+                similarities.append(('target', min_max(self.X[j,indices], self.X[i,indices])))
+                closest.append(max(similarities, key=lambda i: i[1])[0])
+                sigma = closest.count("target") / float(len(closest))
+                sigmas[k] = sigma
+            mean_sigma = sigmas.mean()
+            print "sigma for text: ", mean_sigma
+            same_author = True if mean_sigma >= self.sigma else False
+            print "%s (by %s) same author as %s (by %s) = %s" % (
+                title_i, author_i, title_j, author_j, same_author)
 
     verify = predict
 
