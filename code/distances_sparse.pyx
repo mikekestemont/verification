c55ab47be86eecebd13ac03fd16522a80592d433
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-11-28 16:22:54 +0100

    add sparse divergence

--- code/distances_sparse.pyx
+++ code/distances_sparse.pyx
@@ -10,7 +10,7 @@ cdef fused floating1d:
 @cython.boundscheck(False)
 def sparse_min_max(floating1d X_data, int[:] X_indices, int[:] X_indptr,
                    floating1d Y_data, int[:] Y_indices, int[:] Y_indptr,
-                   int n_features):
+                   int n_features, int[:] indices):
     cdef double mins = 0.0
     cdef double maxs = 0.0
     cdef double[::1] xrow = np.zeros(n_features)
@@ -20,7 +20,7 @@ def sparse_min_max(floating1d X_data, int[:] X_indices, int[:] X_indptr,
         xrow[X_indices[j]] = X_data[j]
     for j in range(Y_indptr[0], Y_indptr[1]):
         yrow[Y_indices[j]] = Y_data[j]
-    for i in range(n_features):
+    for i in indices:
         a = xrow[i]
         b = yrow[i]
         if a >= b:
@@ -32,3 +32,23 @@ def sparse_min_max(floating1d X_data, int[:] X_indices, int[:] X_indptr,
     if maxs > 0.0:
         return 1.0 - (mins / maxs)
     return 0.0
+
+cdef inline double square(double i): return i * i
+
+@cython.boundscheck(False)
+def sparse_divergence(floating1d X_data, int[:] X_indices, int[:] X_indptr,
+                      floating1d Y_data, int[:] Y_indices, int[:] Y_indptr,
+                      int n_features):
+    cdef double[::1] xrow = np.zeros(n_features)
+    cdef double[::1] yrow = np.zeros(n_features)
+    cdef np.npy_intp j, i
+    cdef double divergence = 0.0
+    for j in range(X_indptr[0], X_indptr[1]):
+        xrow[X_indices[j]] = X_data[j]
+    for j in range(Y_indptr[0], Y_indptr[1]):
+        yrow[Y_indices[j]] = Y_data[j]
+    for i in range(n_features):
+        a, b = xrow[i], yrow[i]
+        if a != b:
+        divergence += square((2.0 * (a-b)) / (a+b))
+    return divergence
