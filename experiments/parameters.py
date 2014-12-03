2d4f5a5ad995fb124aff18cdcb3527847a080d95
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-03 12:06:21 +0100

    warn!

--- experiments/parameters.py
+++ experiments/parameters.py
@@ -1,3 +1,7 @@
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.WARN)
+
 import sys
 from itertools import product
 from operator import mul
