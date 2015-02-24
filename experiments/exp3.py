ef40072a690ce58edd67187c7973027cf302f78d
Mike Kestemont <mike.kestemont@gmail.com>
2015-02-24 15:15:43 +0100

    added plot results train data

--- experiments/exp3.py
+++ experiments/exp3.py
@@ -2,7 +2,7 @@
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                    level=logging.INFO)
+                    level=logging.WARNING)
 
 import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
@@ -75,15 +75,17 @@ intervals = 8
 
 print "=== RESAMPLING ==="
 potential_imposter_ranges = [int(i) for i in np.linspace(10, int(len(train_titles)/2), intervals)]
-df = pd.DataFrame(columns=["potential"]+[str(n+1) for n in range(intervals)])
-print df.to_string()
+df_test = pd.DataFrame(columns=["potential"]+[str(n+1) for n in range(intervals)])
+df_train = pd.DataFrame(columns=["potential"]+[str(n+1) for n in range(intervals)])
+
 for i, n_potential_imposters in enumerate(potential_imposter_ranges):
-    row = [str(n_potential_imposters)]
+    test_row = [str(n_potential_imposters)]
+    train_row = [str(n_potential_imposters)]
     print "* nr of potential imposters: "+str(n_potential_imposters)
     n_actual_imposter_ranges = [int(i) for i in np.linspace(1, n_potential_imposters, intervals)]
     for n_actual_imposters in n_actual_imposter_ranges:
         print "\t+ nr of actual imposters: "+str(n_actual_imposters)
-        verifier = Verification(n_features=10000,
+        verifier = Verification(n_features=2500,
                                 random_prop=0.5,
                                 sample_features=True,
                                 sample_authors=True,
@@ -102,21 +104,49 @@ for i, n_potential_imposters in enumerate(potential_imposter_ranges):
         verifier.fit(X_train, X_test)
         train_results, test_results = verifier.verify()
         logging.info("Computing results")
+        # get train results:
         dev_f, dev_p, dev_r, dev_t = evaluate(train_results)
         best_t = dev_t[np.nanargmax(dev_f)]
+        train_f, train_p, train_r = evaluate_with_threshold(train_results, t=best_t)
+        print "\t\t=== train scores ==="
+        print "\t\t- F-score: "+str(train_f)
+        print "\t\t- Precision: "+str(train_p)
+        print "\t\t- Recall: "+str(train_r)
+        train_row.append(train_f)
+        # get test results:
         test_f, test_p, test_r = evaluate_with_threshold(test_results, t=best_t)
-        row.append(test_f)
+        print ("\t\t=== test scores ===")
         print "\t\t- F-score: "+str(test_f)
         print "\t\t- Precision: "+str(test_p)
         print "\t\t- Recall: "+str(test_r)
-    df.loc[i] = row
-    print df.to_string()
-df = df.set_index("potential")
-df.columns.name = "actual imposters"
-df.index.name = "potential imposters"
-df = df.applymap(lambda x:int(x*100))
+        test_row.append(test_f)
+    # update train df:
+    df_train.loc[i] = train_row
+    print "=== train scores ==="
+    print df_train.to_string()
+    # update test df:
+    df_test.loc[i] = test_row
+    print "=== test scores ==="
+    print df_test.to_string()
+# process and plot train df:
+df_train = df_train.set_index("potential")
+df_train.columns.name = "actual imposters"
+df_train.index.name = "potential imposters"
+df_train = df_train.applymap(lambda x:int(x*10000))
 sb.plt.figure()
-sb.heatmap(df, annot=True)
-sb.plt.savefig("../plots/exp3.pdf")
+sb.heatmap(df_train, annot=True)
+sb.plt.savefig("../plots/exp3_train.pdf")
 sb.plt.clf()
-print str(df.to_string())
+print "=== train scores ==="
+print str(df_train.to_string())
+# process and plot train df:
+df_test = df_test.set_index("potential")
+df_test.columns.name = "actual imposters"
+df_test.index.name = "potential imposters"
+df_test = df_test.applymap(lambda x:int(x*10000))
+sb.plt.figure()
+sb.heatmap(df_test, annot=True)
+sb.plt.savefig("../plots/exp3_test.pdf")
+sb.plt.clf()
+print "=== test scores ==="
+print str(df_test.to_string())
